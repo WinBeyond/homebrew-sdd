ef40e6143f0e85de99b83bc1eae2eb2bd98aa35c
masonwwang <masonwwang@tencent.com>
2022-04-26 16:56:40 +0800

    add go

---
+++ Formula/go.rb
@@ -0,0 +1,91 @@
+class Go < Formula
+    desc "Open source programming language to build simple/reliable/efficient software"
+    homepage "https://go.dev/"
+    url "https://go.dev/dl/go1.18.1.src.tar.gz"
+    mirror "https://fossies.org/linux/misc/go1.18.1.src.tar.gz"
+    sha256 "efd43e0f1402e083b73a03d444b7b6576bb4c539ac46208b63a916b69aca4088"
+    license "BSD-3-Clause"
+    head "https://go.googlesource.com/go.git", branch: "master"
+  
+    livecheck do
+      url "https://go.dev/dl/"
+      regex(/href=.*?go[._-]?v?(\d+(?:\.\d+)+)[._-]src\.t/i)
+    end
+  
+    bottle do
+      sha256 arm64_monterey: "1f43c3563aa7d0a124745b4873a3122565761400d6d9e363962a3dbddce980d3"
+      sha256 arm64_big_sur:  "87bbda21351bcc22076014a59c5ad5ea45468748170d5f268e2e4c90140bd7b7"
+      sha256 monterey:       "642ebd8d3e2d3ee1e959aa0af67cd597594498480a55557025f4b1d70d2e9057"
+      sha256 big_sur:        "7c10a52de6e68d572ecca1232dc8ca15c9ae0362fca909105326e32527d30199"
+      sha256 catalina:       "fb1371dd30097b6408a9cf37b26bdf714e449785803aadcdc40b88813e79c90a"
+      sha256 x86_64_linux:   "47b7a9af4e2d45035cf2de9d4a62b1ef04dbf3449ba08e3ef5b6b39be14855fa"
+    end
+  
+    # Don't update this unless this version cannot bootstrap the new version.
+    resource "gobootstrap" do
+      on_macos do
+        if Hardware::CPU.arm?
+          url "https://storage.googleapis.com/golang/go1.16.darwin-arm64.tar.gz"
+          version "1.16"
+          sha256 "4dac57c00168d30bbd02d95131d5de9ca88e04f2c5a29a404576f30ae9b54810"
+        else
+          url "https://storage.googleapis.com/golang/go1.16.darwin-amd64.tar.gz"
+          version "1.16"
+          sha256 "6000a9522975d116bf76044967d7e69e04e982e9625330d9a539a8b45395f9a8"
+        end
+      end
+  
+      on_linux do
+        if Hardware::CPU.arm?
+          url "https://storage.googleapis.com/golang/go1.16.linux-arm64.tar.gz"
+          version "1.16"
+          sha256 "3770f7eb22d05e25fbee8fb53c2a4e897da043eb83c69b9a14f8d98562cd8098"
+        else
+          url "https://storage.googleapis.com/golang/go1.16.linux-amd64.tar.gz"
+          version "1.16"
+          sha256 "013a489ebb3e24ef3d915abe5b94c3286c070dfe0818d5bca8108f1d6e8440d2"
+        end
+      end
+    end
+  
+    def install
+      (buildpath/"gobootstrap").install resource("gobootstrap")
+      ENV["GOROOT_BOOTSTRAP"] = buildpath/"gobootstrap"
+  
+      cd "src" do
+        ENV["GOROOT_FINAL"] = libexec
+        system "./make.bash", "--no-clean"
+      end
+  
+      (buildpath/"pkg/obj").rmtree
+      rm_rf "gobootstrap" # Bootstrap not required beyond compile.
+      libexec.install Dir["*"]
+      bin.install_symlink Dir[libexec/"bin/go*"]
+  
+      system bin/"go", "install", "-race", "std"
+  
+      # Remove useless files.
+      # Breaks patchelf because folder contains weird debug/test files
+      (libexec/"src/debug/elf/testdata").rmtree
+      # Binaries built for an incompatible architecture
+      (libexec/"src/runtime/pprof/testdata").rmtree
+    end
+  
+    test do
+      (testpath/"hello.go").write <<~EOS
+        package main
+        import "fmt"
+        func main() {
+            fmt.Println("Hello World")
+        }
+      EOS
+      # Run go fmt check for no errors then run the program.
+      # This is a a bare minimum of go working as it uses fmt, build, and run.
+      system bin/"go", "fmt", "hello.go"
+      assert_equal "Hello World\n", shell_output("#{bin}/go run hello.go")
+  
+      ENV["GOOS"] = "freebsd"
+      ENV["GOARCH"] = "amd64"
+      system bin/"go", "build", "hello.go"
+    end
+  end
